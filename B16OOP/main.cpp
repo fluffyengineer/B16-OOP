f8bc3a341c01ca8dcdf1621a016e7d1b87317921
Tom Rawlinson <tom@toms-macbook-pro-2.local>
2016-02-18 15:35:21 +0000

    Initial Commit

---
+++ B16OOP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  B16OOP
+//
+//  Created by Tom Rawlinson on 18/02/2016.
+//  Copyright © 2016 Tom Rawlinson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
